e2ba17371f09a65178df652537ff36c90e98e0fe
bluesand <xiong@qianxunmall.com>
2019-04-25 15:03:22 +0800

    modify log

--- core/pipeline/pipeline_console.go
+++ core/pipeline/pipeline_console.go
@@ -3,6 +3,8 @@ package pipeline
 import (
     "github.com/bluesand/go_spider/core/common/com_interfaces"
     "github.com/bluesand/go_spider/core/common/page_items"
+
+    log "github.com/sirupsen/logrus"
 )
 
 type PipelineConsole struct {
@@ -13,10 +15,10 @@ func NewPipelineConsole() *PipelineConsole {
 }
 
 func (this *PipelineConsole) Process(items *page_items.PageItems, t com_interfaces.Task) {
-    println("----------------------------------------------------------------------------------------------")
-    println("Crawled url :\t" + items.GetRequest().GetUrl() + "\n")
-    println("Crawled result : ")
+    log.Debug("----------------------------------------------------------------------------------------------")
+    log.Debug("Crawled url :\t" + items.GetRequest().GetUrl() + "\n")
+    log.Debug("Crawled result : ")
     for key, value := range items.GetAll() {
-        println(key + "\t:\t" + value)
+        log.Debug(key + "\t:\t" + value)
     }
 }
